c336ad1657b0d22bcdde3a7d0cd7b51636bd15e4
lee.gitonga <lee.gitonga@turnkeyafrica.com>
2025-02-26 11:12:50 +0300

    untracked files changed to tracked

---
+++ src/main/java/com/website/RRE/RreApplication.java
@@ -0,0 +1,13 @@
+package com.website.RRE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/website/RRE/RreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.RRE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
